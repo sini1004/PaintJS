f6bbe7c62fcc200945e344da24ec6cc99e1ed0a2
shinhee <tlsgml656@gmail.com>
2022-05-25 00:26:53 +0900

    third commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    painting = false;
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+}
